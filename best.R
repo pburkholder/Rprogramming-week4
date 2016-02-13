6c2209c19ecccd48a9a1c6f4c1d327c3ccbb7c71
Peter Burkholder <pburkholder@chef.io>
2016-02-13 15:25:29 -0500

    Best is returns correct frame

--- best.R
+++ best.R
@@ -1,6 +1,6 @@
 best <- function(state, outcome) {
   ## read outcome data
-  allOutcomes<- read.csv('outcome-of-care-measures.csv')
+  allOutcomes<- read.csv('outcome-of-care-measures.csv', colClasses = "character")
   outcomeByState <- split(allOutcomes, allOutcomes$State)
 
   ## check that state and outcome are valid
@@ -16,7 +16,12 @@ best <- function(state, outcome) {
   if (is.null(valid_outcomes[[outcome]])) {
     stop("invalid outcome")
   }
-  
+
   ## Return hospital name in that state with lowest
   ## 30-day death rate
+  column = valid_outcomes[[outcome]]
+
+  outcomeByState[[state]][,column] <- as.numeric(outcomeByState[[state]][,column])
+  return(outcomeByState[[state]][,column])
+
 }
